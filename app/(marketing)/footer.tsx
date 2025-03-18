8d23628ce59a585dc8358b8946335ad230e8b46f
Rosyidmw <rosyidmktwbw@gmail.com>
2025-03-18 11:13:07 +0700

    feat(footer): add language selection buttons in footer; include SVG icons for multiple languages

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,61 @@
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
+
 export const Footer = () => {
     return (
         <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-            Footer
+            <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+                <Button size="lg" variant="ghost" >
+                    <Image
+                    src="/sa.svg"
+                    alt="Saudi Arabia"
+                    height={32}
+                    width={40}
+                    className="mr-4 rounded-md"
+                    />
+                    Arabic Language
+                </Button>
+                <Button size="lg" variant="ghost" >
+                    <Image
+                    src="/jp.svg"
+                    alt="Japan"
+                    height={32}
+                    width={40}
+                    className="mr-4 rounded-md"
+                    />
+                    Japanese
+                </Button>
+                <Button size="lg" variant="ghost" >
+                    <Image
+                    src="/fr.svg"
+                    alt="France"
+                    height={32}
+                    width={40}
+                    className="mr-4 rounded-md"
+                    />
+                    French
+                </Button>
+                <Button size="lg" variant="ghost" >
+                    <Image
+                    src="/it.svg"
+                    alt="Italy"
+                    height={32}
+                    width={40}
+                    className="mr-4 rounded-md"
+                    />
+                    Italian
+                </Button>
+                <Button size="lg" variant="ghost" >
+                    <Image
+                    src="/en.svg"
+                    alt="England"
+                    height={32}
+                    width={40}
+                    className="mr-4 rounded-md"
+                    />
+                    English
+                </Button>
+            </div>
         </footer>
     )
 }
