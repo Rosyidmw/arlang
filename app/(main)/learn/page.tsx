6dbee1e3b270e22d8a4a68c13e617751a5ee4978
Rosyidmw <rosyidmktwbw@gmail.com>
2025-03-22 09:11:46 +0700

    feat(learn): enhance Learn page layout with FeedWrapper and StickyWrapper components; add UserProgress and Header components

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,23 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { UserProgress } from "@/components/user-progress";
+
+import { Header } from "./header";
+
 const LearnPage = () => {
     return ( 
-        <div>
-            Learn Page
+        <div className="flex flex-row-reverse gap-[48px] px-6">
+            <StickyWrapper>
+                <UserProgress 
+                    activeCourse={{ title: "Arabic", imageSrc: "/sa.svg" }}
+                    hearths={5}
+                    points={100}
+                    hasActiveSubscription={false}
+                />
+            </StickyWrapper>
+            <FeedWrapper>
+                <Header title="Arabic"/>
+            </FeedWrapper>
         </div>
      );
 }
